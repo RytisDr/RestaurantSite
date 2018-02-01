8714de0cda97a03b2f998f8ebfdd5fc285b79cd8
RytisDr <r.drazdauskas@gmail.com>
2018-02-01 22:54:27 +0100

    function to diplay over hover is there, needs finishing

--- script.js
+++ script.js
@@ -1,16 +1,28 @@
 let products = document.querySelectorAll(".product");
+let extra = document.getElementsByClassName("extraInfo");
 
-products.forEach(function(elem){
-elem.addEventListener('mouseover', moreInfo);
-         });
+products.forEach(function (elem) {
+    elem.addEventListener('mouseover', moreInfo);
+    elem.addEventListener('mouseout', hideInfo);
+});
 
-function moreInfo(){
+function moreInfo() {
+
+}
+
+
+function hideInfo() {
+
+}
+/*
+function moreInfo() {
     console.log("work");
     let extra = document.getElementsByClassName("extraInfo");
-    extra.forEach(function(elem){
+    extra.forEach(function (elem) {
         elem.style.display = "inline";
     })
-   /* products[0].addEventListener('mouseout', function(){
+    products[0].addEventListener('mouseout', function () {
         extra[0].style.display = 'none';
-    })*/
+    })
 }
+*/
