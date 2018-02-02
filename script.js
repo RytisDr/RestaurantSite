425dfc367fd3946cf6b90a5ae9d9d0c48ff6286b
RytisDr <r.drazdauskas@gmail.com>
2018-02-02 14:51:31 +0100

    Data imported from the API.

--- script.js
+++ script.js
@@ -1,28 +1,40 @@
-let products = document.querySelectorAll(".product");
+
 let extra = document.getElementsByClassName("extraInfo");
 
-products.forEach(function (elem) {
-    elem.addEventListener('mouseover', moreInfo);
-    elem.addEventListener('mouseout', hideInfo);
-});
 
-function moreInfo() {
 
+function moreInfo(e) {
+    e.currentTarget.querySelector(".extraInfo").style.display="inline";
 }
 
 
-function hideInfo() {
-
+function hideInfo(e) {
+    e.currentTarget.querySelector(".extraInfo").style.display="none";
 }
-/*
-function moreInfo() {
-    console.log("work");
-    let extra = document.getElementsByClassName("extraInfo");
-    extra.forEach(function (elem) {
-        elem.style.display = "inline";
+
+let productlist_link = "http://kea-alt-del.dk/t5/api/productlist";
+let image_path = "http://kea-alt-del.dk/t5/site/imgs/small/";
+let section = document.querySelector("#menu");
+let template = document.querySelector("#temp");
+
+function loadData(link){
+    fetch(link).then(e=>e.json()).then(data=>show(data));
+}
+
+function show(data){
+    data.forEach(element=>{
+        let clone = template.cloneNode(true).content;
+        clone.querySelector('.productImg').src = image_path + element.image + "-sm.jpg";
+        clone.querySelector('.productName').textContent = element.name;
+        clone.querySelector('.price').textContent = element.price;
+
+        console.log(element);
+        section.appendChild(clone);
     })
-    products[0].addEventListener('mouseout', function () {
-        extra[0].style.display = 'none';
-    })
+    let products = document.querySelectorAll(".product");
+    products.forEach(function (elem) {
+        elem.addEventListener('mouseover', moreInfo);
+        elem.addEventListener('mouseout', hideInfo);
+    });
 }
-*/
+loadData(productlist_link);
