7ca16f37c37b383445d49b551e34e26cb5fb8efe
RytisDr <r.drazdauskas@gmail.com>
2018-02-09 10:29:39 +0100

    add details hover (random data)

--- script.js
+++ script.js
@@ -6,15 +6,12 @@ let main = document.querySelector("main");
 let template = document.querySelector("#temp");
 const nav = document.querySelector("nav")
 const fullMenu = document.querySelector("nav a");
-const modal = document.querySelector("#modal");
-
-
-
-
+const modal = document.querySelector(".modal");
 
 
 fetch(catLink).then(result => result.json()).then(data => categorise(data));
 
+/////////////////////////////CATEGORISE INTO SECTIONS/////////////////////////////////////////////////
 function categorise(data) {
     data.forEach(category => {
         const section = document.createElement("section");
@@ -25,10 +22,13 @@ function categorise(data) {
         section.appendChild(h2);
         main.appendChild(section);
         a.textContent = category;
+
+        ///////////////////////////////////FIX SPELLING MISTAKES IN CATEGORIES//////////////////////////////////////////////
         if ((a, h2).textContent == "main" || (a, h2).textContent == "starter" || (a, h2).textContent == "dessert") {
             a.append("s");
             h2.append("s");
         }
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
         a.href = "#";
         a.addEventListener("click", () => filter(category));
         nav.appendChild(a);
@@ -36,15 +36,22 @@ function categorise(data) {
     fetch(productlist_link).then(result => result.json()).then(data => show(data));
 }
 
+
+//////////////////////FILTER INTO CATEGORIES UPON A CLICK////////////////
+
+fullMenu.addEventListener('click', () => filter("all"));
+
 function filter(category) {
     document.querySelectorAll("main section").forEach(section => {
-        if (section.id == category) {
+        if (section.id == category || category == "all") {
             section.classList.remove('hide');
         } else {
             section.classList.add('hide');
         }
     })
 }
+
+///////////////////////////// SHOW DATA /////////////////////////////////////////////
 
 function show(data) {
     data.forEach(element => {
@@ -63,10 +70,11 @@ function show(data) {
             clone.querySelector(".discPrice").classList.remove("hide");
             clone.querySelector(".price").classList.add("strike");
         }
-        fetch(pLink + element.id).then(res => res.json()).then(product => showDetail(product));
+        fetch(pLink + element.id).then(res => res.json()).then(product => showDetail(product)); //STRUGLING TO SHOW CORRECT DATA//////////////////
         section.appendChild(clone);
     })
 
+    ///////////////////////////////////////////SHOW EXTRA INFO FUNCTION WORKS, DOESNT SHOW CORRECT INFO////////////////////
     let products = document.querySelectorAll(".product");
 
     function showDetail(product) {
@@ -74,29 +82,17 @@ function show(data) {
             elem.addEventListener('mouseover', moreInfo);
             elem.addEventListener('mouseout', hideInfo);
         })
-    function moreInfo(e) {
-    e.currentTarget.querySelector(".modal").classList.remove("hide");
-    e.currentTarget.querySelector(".modal-description").textContent = product.longdescription;/*(".extraInfo").style.display = "inline";*/
-}
-        function hideInfo(e) {
-    e.currentTarget.querySelector(".modal").classList.add("hide"); /*(".extraInfo").style.display = "none";*/
-}
 
-
-    };
-
-}
-
-
-
-fullMenu.addEventListener('click', () => filter("all"));
-
-function filter(category) {
-    document.querySelectorAll("main section").forEach(section => {
-        if (section.id == category || category == "all") {
-            section.classList.remove('hide');
-        } else {
-            section.classList.add('hide');
+        function moreInfo(e) {
+            e.currentTarget.querySelector(".modal").classList.remove("hide");
+            e.currentTarget.querySelector(".modal-description").textContent = elem.longdescription; /*(".extraInfo").style.display = "inline";*/
         }
-    })
+
+        function hideInfo(e) {
+            e.currentTarget.querySelector(".modal").classList.add("hide"); /*(".extraInfo").style.display = "none";*/
+        }
+
+
+    }
+
 }
